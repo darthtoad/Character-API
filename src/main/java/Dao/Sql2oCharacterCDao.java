b5d1e2e3af15c85046a662a643ed7146910a1d16
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2018-01-26 15:12:13 -0800

    pass test for updating characters

--- src/main/java/Dao/Sql2oCharacterCDao.java
+++ src/main/java/Dao/Sql2oCharacterCDao.java
@@ -51,7 +51,30 @@ public class Sql2oCharacterCDao implements CharacterCDao {
 
     @Override
     public void update(int id, String name, String description, int level, int experience, int HP, int currentHP, int defense, int magicDefense, int strength, int MP, int currentMP, int magic, int dexterity, String spells, String equipment, String effects) {
-
+        String sql = "UPDATE characters SET name = :name, description = :description, level = :level, experience = :experience, HP = :HP, currentHP = :currentHP, defense = :defense, magicDefense = :magicDefense, strength = :strength, MP = :MP, currentMP = :currentMP, magic = :magic, dexterity = :dexterity, spells = :spells, equipment = :equipment, effects = :effects WHERE id = :id";
+        try (Connection connection = sql2o.open()) {
+            connection.createQuery(sql)
+                    .addParameter("id", id)
+                    .addParameter("name", name)
+                    .addParameter("description", description)
+                    .addParameter("level", level)
+                    .addParameter("experience", experience)
+                    .addParameter("HP", HP)
+                    .addParameter("currentHP", currentHP)
+                    .addParameter("defense", defense)
+                    .addParameter("magicDefense", magicDefense)
+                    .addParameter("strength", strength)
+                    .addParameter("MP", MP)
+                    .addParameter("currentMP", currentMP)
+                    .addParameter("magic", magic)
+                    .addParameter("dexterity", dexterity)
+                    .addParameter("spells", spells)
+                    .addParameter("equipment", equipment)
+                    .addParameter("effects", effects)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
 
     @Override
