d9665afc18ad4d80231ecb835d73fc4b0d6f3a92
Rodnier Borrego <rodnierbc89@gmail.com>
2018-01-30 13:33:08 -0800

    create populateEquipment mathod

--- src/main/java/Dao/Sql2oEquipmentDao.java
+++ src/main/java/Dao/Sql2oEquipmentDao.java
@@ -88,4 +88,41 @@ public class Sql2oEquipmentDao implements EquipmentDao {
             System.out.println(ex);
         }
     }
+
+    @Override
+    public void populateEquipments() {
+        Equipment shortSword = new Equipment("Short Sword", "A short sword", 5, 0, 0, 0, 0);
+        this.add(shortSword);
+        Equipment mythrilKnife = new Equipment("Mythril Knife", "A knife wrought from mythril", 7, 0, 0, 0, 0);
+        this.add(mythrilKnife);
+        Equipment runeBlade = new Equipment("Rune Blade", "A sword effective against spellcasters", 4, 0, 0, 0, 0);
+        this.add(runeBlade);
+        Equipment flameSword = new Equipment("Flame Sword", "A sword that dances with flame", 8, 0, 0, 0, 0);
+        this.add(flameSword);
+        Equipment scimitar = new Equipment("Scimitar", "A sword with a curved blade", 5, 0, 0, 0, 0);
+        this.add(scimitar);
+        Equipment ashura = new Equipment("Ashura", "A sword whose name embodies battle", 6, 0, 0, 0, 0);
+        this.add(ashura);
+        Equipment falchion = new Equipment("Falchion", "A sword that widens toward its tip", 7, 0, 0, 0, 0);
+        this.add(falchion);
+        Equipment saber = new Equipment("Saber", "A long sword made for piercing", 4, 0, 0, 0, 0);
+        this.add(saber);
+        Equipment healingStaff = new Equipment("Healing Staff", "A staff that casts Heal when used", 5, 4, 0, 0, 0);
+        this.add(healingStaff);
+        Equipment goldenStaff = new Equipment("Golden Staff", "A staff that enhances intelligence", 4, 6, 0, 0, 0);
+        this.add(goldenStaff);
+        Equipment judgmentStaff = new Equipment("Judgment Staff", "A staff that casts Flare when used", 7, 5, 0, 0, 0);
+        this.add(judgmentStaff);
+        Equipment ironArmor = new Equipment("Iron Armor", "Armor of iron", 0, 0, 0, 7, 0);
+        this.add(ironArmor);
+        Equipment darkArmor = new Equipment("Dark Armor", "Armor possessing a powerful dark force", 0, 2, 0, 6, 4);
+        this.add(ironArmor);
+        Equipment flameMail = new Equipment("Flame Mail", "Armor immersed in the power of fire, resistant against ice", 0, 0, 0, 8, 3);
+        this.add(flameMail);
+        Equipment iceArmor = new Equipment("Ice Armor", "Armor immersed in the power of ice, resistant against fire", 0, 0, 0, 6, 1);
+        this.add(iceArmor);
+        Equipment diamondArmor = new Equipment("Diamond Armor", "Armor resistant against lightning", 0, 0, 0, 9, 9);
+        this.add(iceArmor);
+
+    }
 }
